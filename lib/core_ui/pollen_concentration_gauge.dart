1f08db3c4bc56b5a27f219721c03d89612989ee4
Mikhail Zadorsky <mzadorskyy@gmail.com>
2023-04-13 16:28:14 +0300

    feat: Add basic gauge impl

---
+++ lib/core_ui/pollen_concentration_gauge.dart
@@ -0,0 +1,67 @@
+import 'package:gauge_indicator/gauge_indicator.dart';
+import 'package:flutter/material.dart';
+
+class PollenConcentrationGauge extends StatelessWidget {
+  const PollenConcentrationGauge(
+      {super.key,
+      required this.concentration,
+      required this.icon,
+      required this.gaugeColor,
+      required this.progressBarColor,
+      required this.thickness,
+      required this.min,
+      required this.max,
+      required this.textColor,
+      required this.fontSize,
+      required this.text});
+
+  final double concentration;
+  final IconData icon;
+  final Color gaugeColor;
+  final Color progressBarColor;
+  final double thickness;
+  final double min;
+  final double max;
+  final Color textColor;
+  final double fontSize;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => Column(
+        children: [
+          RadialGauge(
+            radius: (constraints.maxWidth < constraints.maxHeight
+                    ? constraints.maxWidth
+                    : constraints.maxHeight) /
+                2,
+            value: concentration,
+            progressBar: const GaugeRoundedProgressBar(
+              color: Colors.blue,
+            ),
+            axis: GaugeAxis(
+              min: 0,
+              max: 20,
+              degrees: 270,
+              style: GaugeAxisStyle(
+                thickness: thickness,
+                background: gaugeColor,
+              ),
+            ),
+            child: FittedBox(fit: BoxFit.fill, child: Icon(icon)),
+          ),
+          FittedBox(
+              fit: BoxFit.cover,
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: fontSize,
+                ),
+                textAlign: TextAlign.center,
+              ))
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'core_ui/pollen_concentration_gauge.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -58,7 +59,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+
 
   void _incrementCounter() {
     setState(() {
@@ -67,7 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -104,14 +104,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: const [
+            PollenConcentrationGauge(
+                concentration: 10,
+                icon: Icons.forest,
+                gaugeColor: Colors.red,
+                progressBarColor: Colors.blue,
+                thickness: 50,
+                min: 0,
+                max: 20,
+                fontSize: 20,
+                textColor: Colors.black,
+                text: "OK"),
           ],
         ),
       ),
