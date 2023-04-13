cd4bff8067068286c35141f7859934f6d8bb6900
Vladislav Kheynov <lcastory86@gmail.com>
2023-04-13 17:28:29 +0300

    feat(api): repository and dto done

--- lib/core/data/api_constants.dart
+++ lib/core/data/api_constants.dart
@@ -2,6 +2,3 @@ class ApiConstants {
   static String baseURL = 'https://api.ambeedata.com';
   static String pollenEndpoint = '/latest/pollen/by-lat-lng';
 }
-=======
-}
->>>>>>> dd34670 (feat: add dto for ambee api)
