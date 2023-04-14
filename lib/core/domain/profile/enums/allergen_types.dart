eefa542373d7a6a3925bb36c2e0f313125ada3ea
Vladislav Kheynov <lcastory86@gmail.com>
2023-04-14 21:06:40 +0300

    feat: add saving profile to shared prefs

--- lib/core/domain/profile/enums/allergen_types.dart
+++ lib/core/domain/profile/enums/allergen_types.dart
@@ -1,46 +1,45 @@
-class AllergenTypes {}
-
-enum TreeAllergens implements AllergenTypes {
-  hazel(enName: 'Hazel', ruName: 'Орех'),
-  elm(enName: 'Elm', ruName: 'Вяз'),
-  alder(enName: 'Alder', ruName: 'Ольха'),
-  cottonWood(enName: 'Poplar / Cottonwood', ruName: 'Тополь'),
-  oak(enName: 'Oak', ruName: 'Дуб'),
-  plane(enName: 'Plane', ruName: 'Платан'),
-  birch(enName: 'Birch', ruName: 'Береза'),
-  cypress(enName: 'Cypress', ruName: 'Кипарис'),
-  mulberry(enName: 'Mulberry', ruName: 'Шелковица'),
-  ash(enName: 'Ash', ruName: 'Ясень'),
-  maple(enName: 'Maple', ruName: 'Клен'),
-  casuarina(enName: 'Casuarina', ruName: 'Касуарина'),
-  acacia(enName: 'Acacia', ruName: 'Акация'),
-  myrtaceae(enName: 'Myrtaceae', ruName: 'Миртовые'),
-  willow(enName: 'Willow', ruName: 'Ива'),
-  olive(enName: 'Olive', ruName: 'Оливка'),
-  pine(enName: 'Pine', ruName: 'Сосна');
+enum Allergens {
+  //TODO: переделать
+  hazel(enName: 'Hazel', ruName: 'Орех', type: AllergenType.tree),
+  elm(enName: 'Elm', ruName: 'Вяз', type: AllergenType.tree),
+  alder(enName: 'Alder', ruName: 'Ольха', type: AllergenType.tree),
+  cottonWood(
+      enName: 'Poplar / Cottonwood', ruName: 'Тополь', type: AllergenType.tree),
+  oak(enName: 'Oak', ruName: 'Дуб', type: AllergenType.tree),
+  plane(enName: 'Plane', ruName: 'Платан', type: AllergenType.tree),
+  birch(enName: 'Birch', ruName: 'Береза', type: AllergenType.tree),
+  cypress(enName: 'Cypress', ruName: 'Кипарис', type: AllergenType.tree),
+  mulberry(enName: 'Mulberry', ruName: 'Шелковица', type: AllergenType.tree),
+  ash(enName: 'Ash', ruName: 'Ясень', type: AllergenType.tree),
+  maple(enName: 'Maple', ruName: 'Клен', type: AllergenType.tree),
+  casuarina(enName: 'Casuarina', ruName: 'Касуарина', type: AllergenType.tree),
+  acacia(enName: 'Acacia', ruName: 'Акация', type: AllergenType.tree),
+  myrtaceae(enName: 'Myrtaceae', ruName: 'Миртовые', type: AllergenType.tree),
+  willow(enName: 'Willow', ruName: 'Ива', type: AllergenType.tree),
+  olive(enName: 'Olive', ruName: 'Оливка', type: AllergenType.tree),
+  pine(enName: 'Pine', ruName: 'Сосна', type: AllergenType.tree),
+  //weeds
+  mugwort(enName: 'Mugwort', ruName: 'Полынь', type: AllergenType.weed),
+  chenopod(enName: 'Chenopod', ruName: 'Маревые', type: AllergenType.weed),
+  ragweed(enName: 'Ragweed', ruName: 'Амброзия', type: AllergenType.weed),
+  nettle(enName: 'Nettle', ruName: 'Крапива', type: AllergenType.weed),
+  sedges(enName: 'Sedges', ruName: 'Осока', type: AllergenType.weed),
+  aster(enName: 'Aster', ruName: 'Астровые', type: AllergenType.weed),
+  plantago(enName: 'Plantago', ruName: 'Подорожник', type: AllergenType.weed),
+  rumex(enName: 'Rumex', ruName: 'Щавель', type: AllergenType.weed);
 
   final String enName, ruName;
 
-  const TreeAllergens({
+  final AllergenType type;
+
+  const Allergens({
     required this.enName,
     required this.ruName,
+    required this.type,
   });
 }
 
-enum WeedAllergens {
-  mugwort(enName: 'Mugwort', ruName: 'Полынь'),
-  chenopod(enName: 'Chenopod', ruName: 'Маревые'),
-  ragweed(enName: 'Ragweed', ruName: 'Амброзия'),
-  nettle(enName: 'Nettle', ruName: 'Крапива'),
-  sedges(enName: 'Sedges', ruName: 'Осока'),
-  aster(enName: 'Aster', ruName: 'Астровые'),
-  plantago(enName: 'Plantago', ruName: 'Подорожник'),
-  rumex(enName: 'Rumex', ruName: 'Щавель');
-
-  final String enName, ruName;
-
-  const WeedAllergens({
-    required this.enName,
-    required this.ruName,
-  });
+enum AllergenType {
+  weed,
+  tree,
 }
---
+++ test/profile_data_serialization_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pollen_meter/core/data/profile/local/profile_local_data_source.dart';
+import 'package:pollen_meter/core/data/profile/repository/profile_data_repository_local_impl.dart';
+import 'package:pollen_meter/core/domain/profile/enums/allergen_types.dart';
+import 'package:pollen_meter/core/domain/profile/model/profile_data_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  final data = ProfileDataModel(true, [
+    Allergens.alder,
+    Allergens.ash,
+    Allergens.birch,
+    Allergens.chenopod,
+    Allergens.cypress,
+    Allergens.elm,
+  ]);
+
+  debugPrint(data.toJson() as String?);
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences.setMockInitialValues({});
+
+  final prefs = await SharedPreferences.getInstance();
+  final localStorage = ProfileLocalDataStore(sharedPreferences: prefs);
+  final repository =
+      ProfileDataRepositoryLocalImpl(profileLocalDataSource: localStorage);
+
+  await repository.saveProfile(data);
+  final profile = await repository.getProfile();
+  debugPrint(profile.toString());
+}
