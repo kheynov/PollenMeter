4304b952542914bf5c62f27c5f480eb0071ef1d3
Mikhail Zadorsky <mzadorskyy@gmail.com>
2023-04-14 12:38:21 +0300

    fix: dart 2 compat

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: const Center(
+      body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
