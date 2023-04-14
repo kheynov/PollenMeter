13ae47f1bcdc80deb011c95721d0be1819c42606
Denis Spetsialniy <luwirispok@gmail.com>
2023-04-14 12:33:55 +0300

    feat: create allergen selection widget (#9)
    * fear: add square tile imp
    
    * feat: add pollen tile model
    
    * feat: add pollen selection widget

---
+++ lib/profile/presentation/widgets/pollen_selection.dart
@@ -0,0 +1,69 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:pollen_meter/profile/domain/model/pollen_tile_model.dart';
+import 'package:pollen_meter/profile/presentation/widgets/square_tile.dart';
+
+class PollenSelectionWidget extends StatefulWidget {
+  const PollenSelectionWidget({
+    Key? key,
+    this.spacing = 20,
+    required this.listPollens,
+    required this.onChoiceOfTile,
+    this.crossAxisCount = 3,
+  }) : super(key: key);
+
+  /// Кол-во элементов в строке
+  final int crossAxisCount;
+
+  /// Расстояние между элементами
+  final double spacing;
+
+  /// Список моделек
+  final List<PollenModel> listPollens;
+
+  /// Функция, реагирующая на изменение состояния элементов
+  final Function(PollenModel pollenModel) onChoiceOfTile;
+
+  @override
+  State<PollenSelectionWidget> createState() => _PollenSelectionWidgetState();
+}
+
+class _PollenSelectionWidgetState extends State<PollenSelectionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            childAspectRatio: 1,
+            crossAxisSpacing: widget.spacing,
+            mainAxisSpacing: widget.spacing,
+            crossAxisCount: widget.crossAxisCount,
+          ),
+          itemCount: widget.listPollens.length,
+          itemBuilder: (BuildContext context, int index) {
+            return SquareTileWidget(
+              text: widget.listPollens[index].name,
+              image: widget.listPollens[index].pathImage != null
+                  ? Image.asset(widget.listPollens[index].pathImage!)
+                  : const Icon(
+                      Icons.local_florist,
+                      size: 40,
+                    ),
+              selected: widget.listPollens[index].selected,
+              onTap: (bool selected) {
+                log(widget.listPollens[index].toString(),
+                    name:
+                        'SquareTileWidget - ${widget.listPollens[index].name}');
+                widget.listPollens[index].selected = selected;
+                widget.onChoiceOfTile.call(widget.listPollens[index]);
+                setState(() {});
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/profile/presentation/widgets/square_tile.dart
@@ -0,0 +1,67 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class SquareTileWidget extends StatefulWidget {
+  const SquareTileWidget({
+    Key? key,
+    this.selected = false,
+    required this.text,
+    required this.image,
+    required this.onTap,
+  }) : super(key: key);
+
+  final bool selected;
+  final String text;
+  final Widget image;
+  final Function(bool selected) onTap;
+
+  @override
+  State<SquareTileWidget> createState() => _SquareTileWidgetState();
+}
+
+class _SquareTileWidgetState extends State<SquareTileWidget> {
+  late bool selected;
+
+  @override
+  void initState() {
+    selected = widget.selected;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        selected = !selected;
+        setState(() {});
+        widget.onTap.call(selected);
+        log('$selected', name: 'SquaredTile');
+      },
+      child: AnimatedContainer(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(28),
+          color: widget.selected
+              ? const Color(0xffE8DEF8)
+              : const Color(0xffFEF7FF),
+        ),
+        duration: const Duration(milliseconds: 300),
+        child: Column(
+          children: [
+            widget.image,
+            const Spacer(),
+            Expanded(
+              child: Text(
+                widget.text,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
