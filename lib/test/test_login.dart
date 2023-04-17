1c532ff95934f4a0e4d79b6c62c258b981ba3251
Vladislav Kheynov <lcastory86@gmail.com>
2023-04-17 10:08:35 +0300

    chore: add dependencies

---
+++ lib/test/test_login.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterfire_ui/auth.dart';
+import '../firebase_options.dart';
+
+void main() async {
+  initializeFirebase();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+void initializeFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const SignInScreen(
+      providerConfigs: [
+        EmailProviderConfiguration(),
+        GoogleProviderConfiguration(
+          clientId:
+              '374533370010-15698fjfa6tssse8gumdincoi8uam5lq.apps.googleusercontent.com',
+        ),
+      ],
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
